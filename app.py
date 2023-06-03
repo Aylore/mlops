40a2e55a9b69d04b2574a6ab683e9be8e317f4bb
Aylore <ahmedelghitany.2274@gmail.com>
2023-06-03 13:58:31 +0300

    add logging and post

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+import logging
+from data_models import SamplePostRequest
 
 app = FastAPI()
+logging.basicConfig(level=logging.INFO)
 
 
 @app.get("/")
 def home():
-    return "Fuck you "
+    return "Welcome home "
+
+
+@app.post("/predict")
+def predict(request : SamplePostRequest):
+
+    logging.info("This is an info message.")
+    logging.warning("This is a warning message.")
+    logging.error("This is an error message.")
+    return [request.a  + request.b[0], request.c]
